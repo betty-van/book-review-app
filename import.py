4e253729681a6d2b18aee75cfb9f7f2a716feb79
betty-van <bvan1412@gmail.com>
2019-11-12 19:37:37 -0800

    python problems

--- import.py
+++ import.py
@@ -1,10 +1,7 @@
 import os
 
-from flask import Flask, session, render_template
-from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from sys import argv, exit
 import csv
 
 # Interact with a SQL database
@@ -14,14 +11,18 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 
 # create a 'scoped session' that ensures different users' interactions with the database are kept separate
 db = scoped_session(sessionmaker(bind=engine))
-
-f = open("books.csv")
-reader = csv.reader(f)
+def main():
+    f = open("books.csv")
+    reader = csv.reader(f)
 
 
 # loop gives each column a name
-for isbn, title, author, year in reader: 
-    db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)", {"isbn": isbn, "title": title, "author": author, "year": year }) 
-    print("Added {} book published in {} with ISBN number {} written by {}".format(title, year, isbn, author))
-db.commit()
+    for isbn, title, author, year in reader:
+        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+                    {"isbn": isbn, "title": title, "author": author, "year": year})
+        print(f"Inserted {title} with ISBN number {isbn} published in {year} by {author}")
+    db.commit()
+
+if __name__ == "__main__":
+    main()
 
