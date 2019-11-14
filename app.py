783f7c4193fd97c17bb347ea12dc6935cac276a2
betty-van <bvan1412@gmail.com>
2019-11-13 18:37:45 -0800

    Working search pages and book results, cannot stay logged in yet, cannot display each book as separate link yet

--- app.py
+++ app.py
@@ -24,6 +24,9 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+# Set up a global variable tracking if logged in or not
+isLoggedIn = False
+
 # Rendering template index.html to default url
 @app.route("/")
 def index():
@@ -35,33 +38,55 @@ def register():
     headline="Enter the following details to make an account."
     return render_template("register.html", headline=headline)
 
-@app.route("/search", methods=["POST"])
+@app.route("/search", methods=["POST", "GET"])
 def search():
     name= request.form.get("name")
     password= request.form.get("password")
-    headline="Fill out the form below to start searching for books."
-    return render_template("search.html", headline=headline, name=name,  password=password)
+    
+    # Check if name and password are already in the database
+    isLoggedIn = db.execute("SELECT name, password FROM users WHERE name = :name AND password = :password", {"name": name, "password": password}).rowcount > 0;
+    if isLoggedIn == True: 
+        # If match, render search page and continue
+        headline="Fill out the form below to start searching for books."
+        return render_template("search.html", headline=headline)
+        isLoggedIn = True
+    elif isLoggedIn == False:
+        # If name and password is NOT in the table, revert to login page
+        headline="You entered an invalid username and password. Please try again or register for an account."
+        return render_template("index.html", headline=headline)  
 
 @app.route("/logout")
 def logout():
     headline="You have successfully logged out."
+    isLoggedIn = False
     return render_template("logout.html", headline=headline)
 
+# TODO
 @app.route("/results", methods=["POST", "GET"])
 def results():
+    isLoggedIn = True
     isbn=request.form.get("isbn")
     title=request.form.get("title")
     author=request.form.get("author")
-    headline="Your search results are below."
-    return render_template("search-result.html", headline=headline, isbn=isbn, title=title, author=author)
 
+    # List all books found in the db that matches
+    books =  db.execute("SELECT * FROM books WHERE isbn = :isbn OR title = :title OR author = :author", {"isbn": isbn, "title": title, "author": author}).fetchall()
+    bookNotFound = db.execute("SELECT * FROM books WHERE isbn = :isbn OR title = :title OR author = :author", {"isbn": isbn, "title": title, "author": author}).rowcount == 0
+    
+    if bookNotFound:
+        headline ="No book was found."
+        return render_template("error.html", headline=headline)
+    else: 
+        headline="Your search results are below."
+        return render_template("search-result.html", headline=headline, isbn=isbn, title=title, author=author, books=books)
+        
 @app.route("/registrationsuccess", methods=["POST"])
 def is_registered():
     name= request.form.get("name")
     password= request.form.get("password")
 
     # Check to see if name and password is already in the table
-    alreadyRegistered = db.execute("SELECT name, password FROM users WHERE name = :name AND password = :password", {"name": name, "password": password}).rowcount > 0;
+    alreadyRegistered = db.execute("SELECT name, password FROM users WHERE name = :name AND password = :password", {"name": name, "password": password}).rowcount > 0
     if alreadyRegistered: 
         headline="You already have an account. Please log in by clicking the link below to be redirected back to the home page."
         return render_template("registrationsuccess.html", headline=headline, name=name,  password=password)
@@ -72,5 +97,7 @@ def is_registered():
         db.commit()
         headline="Successful registration. Please log in by clicking the link below to be redirected back to the home page."
         return render_template("registrationsuccess.html", headline=headline, name=name,  password=password)
-    db.commit()    
+    db.commit()   
+
+
     
